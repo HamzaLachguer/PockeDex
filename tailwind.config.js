a9359a51042f7e4283cb08a31727404cdc714ff5
hamzalachguer <lachguerhmza@gmail.com>
2025-08-01 00:20:31 +0200

    The starter files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        //
+      },
+    },
+  },
+  plugins: [],
+}
